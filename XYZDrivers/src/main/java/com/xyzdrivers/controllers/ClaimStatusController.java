c14e1092967de2a3c549c4c81cb524fb97a38a0e
bkneis <b.kneis2011@gmail.com>
2017-11-12 17:39:18 +0000

    Implement controllers to resume / suspend claim or membership

---
+++ XYZDrivers/src/main/java/com/xyzdrivers/controllers/ClaimStatusController.java
@@ -0,0 +1,100 @@
+package com.xyzdrivers.controllers;
+
+import com.xyzdrivers.models.Claim;
+import com.xyzdrivers.repositories.ClaimsRepo;
+import com.xyzdrivers.services.ResponseService;
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author arthur
+ */
+public class ClaimStatusController extends HttpServlet {
+    
+    @Inject
+    ClaimsRepo claimsRepo;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        // Get the http request params
+        String status = request.getParameter("status");
+        Integer id = Integer.parseInt(request.getParameter("claim_id"));
+        
+        // Sanity check to ensure required parameters were required, if not respond with failure
+        if (status == null || status.equals("")) {
+            ResponseService.fail(request, response, "Failure. Please submit a status and claim id", "/admin");
+            return;
+        }
+        
+        // Get the claim to be updated by id
+        Claim claim = claimsRepo.get(id);
+        
+        // Set the status using model's setter to ensure status is valid, if not respond with failure
+        if (! claim.setStatus(status)) {
+            ResponseService.fail(request, response, "Failure. Please submit a valid status", "/admin");
+            return;
+        }
+        
+        // Update the model and persist to DB
+        claim = claimsRepo.update(claim);
+        
+        // Respond with success
+        ResponseService.success(request, response, "Success. The claim status has been updated to " + claim.getStatus(), "/admin");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ XYZDrivers/src/main/java/com/xyzdrivers/controllers/MembershipStatusController.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xyzdrivers.controllers;
+
+import com.xyzdrivers.models.Membership;
+import com.xyzdrivers.repositories.MembershipRepo;
+import com.xyzdrivers.services.ResponseService;
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author arthur
+ */
+public class MembershipStatusController extends HttpServlet {
+    
+    @Inject
+    MembershipRepo memberRepo;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String status = request.getParameter("status");
+        Integer id = Integer.parseInt(request.getParameter("member_id"));
+        
+        if (status == null || status.equals("")) {
+            ResponseService.fail(request, response, "Failure. Please submit a status and membership id", "/admin");
+            return;
+        }
+        
+        Membership membership = memberRepo.get(id);
+        
+        if (! membership.setStatus(status)) {
+            ResponseService.fail(request, response, "Failure. Please submit a valid status", "/admin");
+            return;
+        }
+        
+        ResponseService.success(request, response, "Success. The membership status has been updated to " + status, "/admin");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
